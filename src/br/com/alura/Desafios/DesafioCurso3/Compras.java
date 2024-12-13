8dcedf5ca2db37aa6b9edfef79c8de8dac3728bd
dutkercz <dutkercz@gmail.com>
2024-12-13 01:22:54 -0300

    Calculo MMC Added var request. Adjust getters and setters criado um comparador p/ordenar a lista.

--- src/br/com/alura/Desafios/DesafioCurso3/Compras.java
+++ src/br/com/alura/Desafios/DesafioCurso3/Compras.java
@@ -1,23 +1,24 @@
 package br.com.alura.Desafios.DesafioCurso3;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class Compras {
+public class Compras{
     public static void main(String[] args) {
 
         Scanner leitor = new Scanner(System.in);
-        List<Bagui> listaDeCompras = new ArrayList<>();
+        List<ClasseCompras> listaDeCompras = new ArrayList<>();
 
         System.out.println("Digite o limite do cartão:");
         double limite = leitor.nextInt();
         System.out.println("Você informou o limite de :"+limite);
 
         while (true){
-            System.out.println("O que deseja fazer:" +
-                    "\n 1 - Comprar." +
-                    "\n 2 - Sair");
+            System.out.println("""
+                    O que deseja fazer:\
+                    
+                     1 - Comprar.\
+                    
+                     2 - Sair""");
 
             int opcao = leitor.nextInt();
 
@@ -31,7 +32,7 @@ public class Compras {
                 double valor = leitor.nextDouble();
 
                 if (valor <= limite){
-                    Bagui precoValor = new Bagui(compra, valor);
+                    ClasseCompras precoValor = new ClasseCompras(compra, valor);
                     listaDeCompras.add(precoValor);
                     limite-=valor;
                     System.out.printf("%s adicionado a lista de compras.\n", compra);
@@ -45,9 +46,24 @@ public class Compras {
             } else if (opcao == 2) {
                 break;
             }
+            else{
+                System.out.println("Opção Inválida!");
+            }
 
         }
+        //Ordenando a lista pelo valor inteiro.
+        //pode utilizar nomeDaLista.sorte(nomeDaLista, new Comparator<NomeDaClasse>(){}
+
+        Collections.sort(listaDeCompras, new Comparator<ClasseCompras>() {
+            @Override
+            public int compare(ClasseCompras i1, ClasseCompras i2) {
+                return Integer.compare((int) i1.preco, (int) i2.preco);//cast de double p/ int - comprando pelo valor
+            }
+        });
         System.out.println("Sua Lista de compras");
-        System.out.println(listaDeCompras.getFirst());
-    }
+        for (ClasseCompras listaDeCompra : listaDeCompras) {
+            System.out.println("Produto:" + listaDeCompra);
+
+        }
+     }
 }
